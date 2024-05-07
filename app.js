e7448645006a3ae3c9ab5b147a661239304290a9
Gabriel Acosta <gabriacos@gmail.com>
2024-05-07 10:10:13 -0300

    Feat: modificación del titutlo

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ function drawCharts() {
 }
 
 function drawChart1() {
-  var data = google.visualization.arrayToDataTable([
+  let data = google.visualization.arrayToDataTable([
     ['Mes', 'Cantidad'],
     ['ENERO', 108],
     ['FEBRERO', 20],
@@ -22,19 +22,19 @@ function drawChart1() {
     ['NOVIEMBRE', 20]
   ]);
 
-  var options = {
+  let options = {
     title: 'Cantidad de Crímenes por Mes',
     hAxis: { title: 'Mes' },
     vAxis: { title: 'Cantidad de Crímenes' },
     legend: 'none'
   };
 
-  var chart = new google.visualization.ColumnChart(document.getElementById('chart_container1'));
+  let chart = new google.visualization.ColumnChart(document.getElementById('chart_container1'));
   chart.draw(data, options);
 }
 
 function drawChart2() {
-  var data = google.visualization.arrayToDataTable([
+  let data = google.visualization.arrayToDataTable([
     ['Día', 'Cantidad'],
     ['LUNES', 37],
     ['MARTES', 40],
@@ -43,17 +43,17 @@ function drawChart2() {
     ['VIERNES', 41]
   ]);
 
-  var options = {
+  let options = {
     title: 'Cantidad de Crímenes por Día de la Semana',
     pieHole: 0.4,
   };
 
-  var chart = new google.visualization.PieChart(document.getElementById('chart_container2'));
+  let chart = new google.visualization.PieChart(document.getElementById('chart_container2'));
   chart.draw(data, options);
 }
 
 function drawChart3() {
-  var data = google.visualization.arrayToDataTable([
+  let data = google.visualization.arrayToDataTable([
     ['Barrio', 'Cantidad'],
     ['CHACARITA', 14],
     ['BARRACAS', 10],
@@ -62,49 +62,49 @@ function drawChart3() {
     ['NUÑEZ', 3]
   ]);
 
-  var options = {
+  let options = {
     title: 'Cantidad de Crímenes por Barrio',
     vAxis: { title: 'Barrio' },
     hAxis: { title: 'Cantidad de Crímenes' },
     legend: 'none'
   };
 
-  var chart = new google.visualization.BarChart(document.getElementById('chart_container3'));
+  let chart = new google.visualization.BarChart(document.getElementById('chart_container3'));
   chart.draw(data, options);
 }
 
 function drawChart4() {
-  var data = google.visualization.arrayToDataTable([
+  let data = google.visualization.arrayToDataTable([
     ['Tipo de Crimen', 'Cantidad'],
     ['Robo total', 124],
     ['Hurto total', 193],
     ['Robo automotor', 131]
   ]);
 
-  var options = {
+  let options = {
     title: 'Cantidad de Crímenes por Tipo de Crimen',
     hAxis: { title: 'Tipo de Crimen' },
     vAxis: { title: 'Cantidad de Crímenes' },
     legend: 'none'
   };
 
-  var chart = new google.visualization.ColumnChart(document.getElementById('chart_container4'));
+  let chart = new google.visualization.ColumnChart(document.getElementById('chart_container4'));
   chart.draw(data, options);
 }
 
 function drawChart5() {
-  var data = google.visualization.arrayToDataTable([
+  let data = google.visualization.arrayToDataTable([
     ['Tipo de Crimen', 'Porcentaje'],
     ['Robo total', 27.68],
     ['Hurto total', 43.08],
     ['Robo automotor', 29.24]
   ]);
 
-  var options = {
+  let options = {
     title: 'Porcentaje de Crímenes por Tipo de Crimen',
     pieHole: 0.4,
   };
 
-  var chart = new google.visualization.PieChart(document.getElementById('chart_container5'));
+  let chart = new google.visualization.PieChart(document.getElementById('chart_container5'));
   chart.draw(data, options);
 }
